6a927b2a077ba8a767bd21abcfbf1c4e3d1b11e0
Andrei Popescu <andreip@google.com>
2010-04-21 13:05:17 +0100

    Re-enable snapshots in V8.
    Fix bug:  2612919
    
    Change-Id: I8c3de4e0df95c9339aadd90e8367239911fe1cee

--- Android.mk
+++ Android.mk
@@ -30,6 +30,7 @@ include $(CLEAR_VARS)
 # Build libv8 and v8shell
 # TODO: remove the check when the v8 build is fixed for x86
 ifeq ($(TARGET_ARCH),arm)
+    ENABLE_V8_SNAPSHOT = true
     include $(BASE_PATH)/Android.mksnapshot.mk
     include $(BASE_PATH)/Android.libv8.mk
     include $(BASE_PATH)/Android.v8shell.mk
