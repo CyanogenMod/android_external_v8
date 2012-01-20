e0d0d1af6e1079cf26f91d88af494ade0e786cbc
Steve Block <steveblock@google.com>
2012-01-20 16:06:48 +0000

    Use stlport for libv8 and v8shell
    Currently we incorrectly use the STL implementation from the toolchain.
    
    Change-Id: Icc337d19e46188a818b07ceab7be7721ebf12ffa

--- Android.v8shell.mk
+++ Android.v8shell.mk
@@ -1,13 +1,16 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_SHARED_LIBRARIES := libstlport
+include external/stlport/libstlport.mk
+
 LOCAL_SRC_FILES:= samples/shell.cc
 LOCAL_MODULE := v8shell
 
 LOCAL_CPP_EXTENSION := .cc
 
 LOCAL_STATIC_LIBRARIES := libv8
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SHARED_LIBRARIES += liblog
 
 LOCAL_MODULE_TAGS := optional
 
