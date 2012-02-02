e7d60fe8c8ca03dbd9c2fda842f83143829054fa
Steve Block <steveblock@google.com>
2012-02-02 18:13:33 +0000

    Revert "Only test for ARM v7 when doing an ARM build"
    This reverts commit c82d6571ba7c10ab287e59968cdd6fe7aa68751a and
    commit d7a34d91def077ae0438f710f157e30f38539abe.
    
    This broke the cts build, which uses ARM v5.
    
    Bug: 5495373
    Change-Id: I2ce84d2bee5a4f710d88ffda631882324f8b86da

--- Android.mk
+++ Android.mk
@@ -18,12 +18,17 @@
 BASE_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+# Two ways to control which JS engine is used:
+# 1. use JS_ENGINE environment variable, value can be either 'jsc' or 'v8'
+#    This is the preferred way.
+# 2. if JS_ENGINE is not set, or is not 'jsc' or 'v8', this makefile picks
+#    up a default engine to build.
+#    To help setup buildbot, a new environment variable, USE_ALT_JS_ENGINE,
+#    can be set to true, so that two builds can be different but without
+#    specifying which JS engine to use.
+
 # Build libv8 and v8shell
 ifeq ($(TARGET_ARCH),arm)
-    ifneq ($(strip $(ARCH_ARM_HAVE_ARMV7A)),true)
-        $(error V8 requires ARM v7)
-    endif
-
     ENABLE_V8_SNAPSHOT = true
     include $(BASE_PATH)/Android.mksnapshot.mk
     include $(BASE_PATH)/Android.libv8.mk
