45d4c017bd11d9e7cce24bf3986e4e6451f60103
Colin Cross <ccross@android.com>
2014-02-12 16:56:28 -0800

    v8: support multilib builds
    Allow building for multiple architectures at the same time:
    Use $(local-generated-sources-dir) instead of $(local-intermediates-dir)
    Set LOCAL_CFLAGS_arch instead of ifeq ($(TARGET_ARCH),arch)
    Use LOCAL_MODULE_TARGET_ARCH_WARN to restrict which architectures
    attempt to build, and print a warning on the rest.
    
    Change-Id: I66dffb36557aa47c92a1ae158683a27e9ca83e53

--- Android.mksnapshot.mk
+++ Android.mksnapshot.mk
@@ -1,78 +1,75 @@
 ##
 # mksnapshot
 # ===================================================
+
+ifneq (,$(filter $(mksnapshot_arch),$(V8_SUPPORTED_ARCH)))
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 # Set up the target identity
 LOCAL_IS_HOST_MODULE := true
-LOCAL_MODULE := mksnapshot.$(TARGET_ARCH)
+LOCAL_MODULE := mksnapshot.$(mksnapshot_arch)
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS = optional
-intermediates := $(call local-intermediates-dir)
+generated_sources := $(call local-generated-sources-dir)
 
-V8_LOCAL_SRC_FILES :=
+
 V8_LOCAL_JS_LIBRARY_FILES :=
 V8_LOCAL_JS_EXPERIMENTAL_LIBRARY_FILES :=
 include $(LOCAL_PATH)/Android.v8common.mk
 
-V8_LOCAL_SRC_FILES += \
+LOCAL_SRC_FILES += \
   src/mksnapshot.cc \
   src/snapshot-empty.cc
 
-ifeq ($(TARGET_ARCH),arm)
-V8_LOCAL_SRC_FILES += src/arm/simulator-arm.cc
-endif
+LOCAL_SRC_FILES_arm += src/arm/simulator-arm.cc
 
-ifeq ($(TARGET_ARCH),mips)
-V8_LOCAL_SRC_FILES += src/mips/simulator-mips.cc
+LOCAL_SRC_FILES_mips += src/mips/simulator-mips.cc
 
-endif
 
 ifeq ($(HOST_ARCH),x86)
-V8_LOCAL_SRC_FILES += src/atomicops_internals_x86_gcc.cc
+LOCAL_SRC_FILES += src/atomicops_internals_x86_gcc.cc
 endif
 
 ifeq ($(HOST_OS),linux)
-V8_LOCAL_SRC_FILES += \
+LOCAL_SRC_FILES += \
   src/platform-linux.cc \
   src/platform-posix.cc
 endif
 ifeq ($(HOST_OS),darwin)
-V8_LOCAL_SRC_FILES += \
+LOCAL_SRC_FILES += \
   src/platform-macos.cc \
   src/platform-posix.cc
 endif
 
-LOCAL_SRC_FILES := $(V8_LOCAL_SRC_FILES)
-
 LOCAL_JS_LIBRARY_FILES := $(addprefix $(LOCAL_PATH)/, $(V8_LOCAL_JS_LIBRARY_FILES))
 LOCAL_JS_EXPERIMENTAL_LIBRARY_FILES := $(addprefix $(LOCAL_PATH)/, $(V8_LOCAL_JS_EXPERIMENTAL_LIBRARY_FILES))
 
-# Copy js2c.py to intermediates directory and invoke there to avoid generating
-# jsmin.pyc in the source directory
-JS2C_PY := $(intermediates)/js2c.py $(intermediates)/jsmin.py
-$(JS2C_PY): $(intermediates)/%.py : $(LOCAL_PATH)/tools/%.py | $(ACP)
+# Copy js2c.py to generated sources directory and invoke there to avoid
+# generating jsmin.pyc in the source directory
+JS2C_PY := $(generated_sources)/js2c.py $(generated_sources)/jsmin.py
+$(JS2C_PY): $(generated_sources)/%.py : $(LOCAL_PATH)/tools/%.py | $(ACP)
 	@echo "Copying $@"
 	$(copy-file-to-target)
 
 # Generate libraries.cc
-GEN3 := $(intermediates)/libraries.cc
-$(GEN3): SCRIPT := $(intermediates)/js2c.py
+GEN3 := $(generated_sources)/libraries.cc
+$(GEN3): SCRIPT := $(generated_sources)/js2c.py
 $(GEN3): $(LOCAL_JS_LIBRARY_FILES) $(JS2C_PY)
 	@echo "Generating libraries.cc"
 	@mkdir -p $(dir $@)
 	python $(SCRIPT) $(GEN3) CORE off $(LOCAL_JS_LIBRARY_FILES)
-LOCAL_GENERATED_SOURCES := $(intermediates)/libraries.cc
+LOCAL_GENERATED_SOURCES := $(generated_sources)/libraries.cc
 
 # Generate experimental-libraries.cc
-GEN4 := $(intermediates)/experimental-libraries.cc
-$(GEN4): SCRIPT := $(intermediates)/js2c.py
+GEN4 := $(generated_sources)/experimental-libraries.cc
+$(GEN4): SCRIPT := $(generated_sources)/js2c.py
 $(GEN4): $(LOCAL_JS_EXPERIMENTAL_LIBRARY_FILES) $(JS2C_PY)
 	@echo "Generating experimental-libraries.cc"
 	@mkdir -p $(dir $@)
 	python $(SCRIPT) $(GEN4) EXPERIMENTAL off $(LOCAL_JS_EXPERIMENTAL_LIBRARY_FILES)
-LOCAL_GENERATED_SOURCES += $(intermediates)/experimental-libraries.cc
+LOCAL_GENERATED_SOURCES += $(generated_sources)/experimental-libraries.cc
 
 LOCAL_CFLAGS := \
 	-Wno-endif-labels \
@@ -86,31 +83,28 @@ LOCAL_CFLAGS := \
 	-DV8_NATIVE_REGEXP \
 	-Wno-unused-parameter
 
-ifeq ($(TARGET_ARCH),arm)
-  LOCAL_CFLAGS += -DV8_TARGET_ARCH_ARM
+LOCAL_CFLAGS_arm += -DV8_TARGET_ARCH_ARM
+
+ifeq ($(ARCH_ARM_HAVE_VFP),true)
+    LOCAL_CFLAGS_arm += -DCAN_USE_VFP_INSTRUCTIONS -DCAN_USE_ARMV7_INSTRUCTIONS
 endif
 
-ifeq ($(TARGET_CPU_ABI),armeabi-v7a)
-    ifeq ($(ARCH_ARM_HAVE_VFP),true)
-        LOCAL_CFLAGS += -DCAN_USE_VFP_INSTRUCTIONS -DCAN_USE_ARMV7_INSTRUCTIONS
-    endif
-endif
+LOCAL_CFLAGS_mips += -DV8_TARGET_ARCH_MIPS
+LOCAL_CFLAGS_mips += -DCAN_USE_FPU_INSTRUCTIONS
+LOCAL_CFLAGS_mips += -Umips
+LOCAL_CFLAGS_mips += -finline-limit=64
+LOCAL_CFLAGS_mips += -fno-strict-aliasing
 
-ifeq ($(TARGET_ARCH),mips)
-  LOCAL_CFLAGS += -DV8_TARGET_ARCH_MIPS
-  LOCAL_CFLAGS += -DCAN_USE_FPU_INSTRUCTIONS
-  LOCAL_CFLAGS += -Umips
-  LOCAL_CFLAGS += -finline-limit=64
-  LOCAL_CFLAGS += -fno-strict-aliasing
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-  LOCAL_CFLAGS += -DV8_TARGET_ARCH_IA32
-endif
+LOCAL_CFLAGS_x86 += -DV8_TARGET_ARCH_IA32
 
 ifeq ($(DEBUG_V8),true)
 	LOCAL_CFLAGS += -DDEBUG -UNDEBUG
 endif
+
+# LOCAL_SRC_FILES_arch only applies to target modules, but we want them
+# for a host module, so append them manually
+LOCAL_SRC_FILES += $(LOCAL_SRC_FILES_$(mksnapshot_arch))
+LOCAL_CFLAGS += $(LOCAL_CFLAGS_$(mksnapshot_arch))
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 
@@ -120,3 +114,6 @@ LOCAL_LDLIBS := -lpthread
 LOCAL_STATIC_LIBRARIES := liblog
 
 include $(BUILD_HOST_EXECUTABLE)
+else
+$(warning mksnapshot.$(mksnapshot_arch): architecture $(mksnapshot_arch) not supported)
+endif
